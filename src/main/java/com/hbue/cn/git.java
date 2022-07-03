25d4dda08397879323de3f7ca5de08c50aedac9f
zy123 <1554267532@qq.com>
2022-07-03 17:36:19 +0800

    five commit

--- src/main/java/com/hbue/cn/git.java
+++ src/main/java/com/hbue/cn/git.java
@@ -7,5 +7,7 @@ public class git {
         System.out.println("my text");
         System.out.println("hello yyyy");
         System.out.println("hello world1");
+        System.out.println("hot-fix test");
+
     }
 }
